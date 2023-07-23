39e5a6adc01a845b13cb6bd5ec4f887bea551532
NyangoyaG <nyangoyagb@gmail.com>
2023-07-23 06:43:59 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-from fabric.api import local, run, lcd
-from datetime import datetime
+"""
+a Fabric script that generates a .tgz archive
+from the contents of the web_static folder of the AirBnB Clone repo
+"""
+from fabric.operations import local
+from datetime import datetime as d
+
 
 def do_pack():
-    # Get the current timestamp for the archive name
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    # Define the archive name and path
-    archive_name = "web_static_{}.tgz".format(timestamp)
-    archive_path = "versions/{}".format(archive_name)
-
-    # Create the versions folder if it doesn't exist
-    local("mkdir -p versions")
-
-    # Compress the contents of web_static into the archive
-    result = local("tar -czvf {} web_static".format(archive_path))
-
-    # Check if the compression was successful
-    if result.succeeded:
-        return archive_path
-    else:
+    """ generates a .tgz archive """
+    name = "versions/web_static_" + str(d.now().year)
+    name += str(d.now().month) + str(d.now().day) + str(d.now().hour)
+    name += str(d.now().minute) + str(d.now().second) + ".tgz"
+    result = local("mkdir -p versions; tar -cvzf \"%s\" web_static" % name)
+    if result.failed:
         return None
+    else:
+        return name
